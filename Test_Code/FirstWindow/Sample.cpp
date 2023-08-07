c5e1fb60eac42be79648d8ab5b3c63a2e93f1c3f
TrueCircular <orijin0322@gmail.com>
2023-08-07 18:53:08 +0900

    sd

--- Test_Code/FirstWindow/Sample.cpp
+++ Test_Code/FirstWindow/Sample.cpp
@@ -1,8 +1,46 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
+    WNDCLASSEXW wcex;
+    ZeroMemory(&wcex, sizeof(wcex));
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.hInstance = hInstance;
+    wcex.lpszClassName = L"KGCA윈도우";
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    WORD ret = RegisterClassExW(&wcex);
 
+    HWND hWnd = CreateWindowW(L"KGCA윈도우", L"타이틀", WS_OVERLAPPEDWINDOW,
+        0, 0, 800, 600, nullptr, nullptr, hInstance, nullptr);
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+    ShowWindow(hWnd, nCmdShow);
+
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+       TranslateMessage(&msg);
+       DispatchMessage(&msg);
+    }
+
+    //::MessageBox(NULL, L"내용", L"타이틀", MB_OK);
 
     return 0;
 }
