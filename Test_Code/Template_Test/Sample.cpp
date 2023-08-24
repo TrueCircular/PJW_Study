1f201a1ef56f8d908db076f39e44499c6c94f568
TrueCircular <orijin0322@gmail.com>
2023-08-24 13:49:54 +0900

    dd

--- Test_Code/Template_Test/Sample.cpp
+++ Test_Code/Template_Test/Sample.cpp
@@ -1,14 +1,16 @@
 #include "Sample.h"
-float g_fMapHalfSizeX = 800;
-float g_fMapHalfSizeY = 450;
+float g_fMapHalfSizeX = 384;
+float g_fMapHalfSizeY = 288;
 
 bool Sample::Init()
 {
+    CreateBlendState();
+
 	m_pMapObj = new TPlaneObj;
 	m_pMapObj->Set(m_pDevice, m_pImmediateContext);
 	m_pMapObj->SetPos({ 0.0f,0.0f ,0.0f });
 	m_pMapObj->SetScale(TVector3(g_fMapHalfSizeX, g_fMapHalfSizeY, 1.0f));
-	m_pMapObj->Create(L"../../resource/map01.png", L"../../resource/shader/Plane.hlsl");
+	m_pMapObj->Create(L"../../resource/topdownmap.jpg", L"../../resource/shader/Plane.hlsl");
 
 	m_MainCamera.Create({ 0.0f,0.0f, 0.0f }, { (float)m_dwWindowWidth, (float)m_dwWindowHeight });
 	return true;
@@ -19,6 +21,7 @@ bool Sample::Frame()
 }
 bool Sample::Render()
 {
+    m_pImmediateContext->OMSetBlendState(m_AlphaBlend, 0, -1);
 	m_pMapObj->SetMatrix(nullptr, &m_MainCamera.m_matView, &m_MainCamera.m_matOrthoProjection);
 	m_pMapObj->Render();
 	
@@ -31,5 +34,33 @@ bool Sample::Release()
 	return true;
 }
 
+void Sample::CreateBlendState()
+{
+    // alpha blending
+    D3D11_BLEND_DESC bsd;
+    ZeroMemory(&bsd, sizeof(bsd));
+    bsd.RenderTarget[0].BlendEnable = true;
+    bsd.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;
+    bsd.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
+    bsd.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+    // 알파블랜딩 공식 - 소스(float4(0,1,0,0.5f)), 대상(1,0,0,1)
+    //finalcolor(RGB) = SrcColor* SrcBlend   + DestColor*DestBlend
+    //           = Scrcolor* alphaValue + DestColor * (1.0f-alphaValue)
+    //           = 0,1,0 * 0.5f + 1,0,0 * (1.0f-0.5f)
+    // //        만약 t= 0.0f; 결과 => 배경만 나온다.
+    // //        만약 t= 1.0f; 결과 => 소스만 나온다.
+    //           만약 t= 0.5f; 결과 - >소스컬러*0.5 + 배경컬러*0.5f
+    //           = 0,1,0 * t + 1,0,0 * (1.0f-t)
+    //           =  r*t,b*t,b*t + r*(1.0f-t),b*(1.0f-t),b*(1.0f-t)
 
-TGAME("KGCA", 1600, 900);
+    // (A)
+    bsd.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+    bsd.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
+    bsd.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;
+
+    bsd.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+    m_pDevice->CreateBlendState(&bsd, &m_AlphaBlend);
+}
+
+
+TGAME("KGCA", 768, 576);
--- Test_Code/Template_Test/Sample.h
+++ Test_Code/Template_Test/Sample.h
@@ -5,11 +5,15 @@
 class Sample : public TCore
 {
 	TObject* m_pMapObj = nullptr;
+	ID3D11BlendState* m_AlphaBlend = nullptr;
 
 public:
 	bool Init();
 	bool Frame();
 	bool Render();
 	bool Release();
+public:
+	void  CreateBlendState();
+
 };
 
