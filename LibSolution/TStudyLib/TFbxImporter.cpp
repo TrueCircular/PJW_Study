6d01deae3c6cd0822a0d71c0530337241448aca1
TrueCircular <orijin0322@gmail.com>
2023-09-22 19:29:57 +0900

    dd

--- LibSolution/TStudyLib/TFbxImporter.cpp
+++ LibSolution/TStudyLib/TFbxImporter.cpp
@@ -51,7 +51,11 @@ void TFbxImporter::LoadMesh(FbxNode* node, TFbxMesh& mesh)
 	FbxVector4* _vertexPos = _mesh->GetControlPoints();
 
 	UINT _layerCount = _mesh->GetLayerCount();
-	std::vector<FbxLayerElementUV*> _vertexUVList;
+
+	std::vector<FbxLayerElementVertexColor*> _vertexColorList;
+	std::vector<FbxLayerElementNormal*>      _vertexNormalList;
+	std::vector<FbxLayerElementUV*>			 _vertexUVList;
+	std::vector<FbxLayerElementMaterial*>    _materialList;
 
 	for (int i = 0; i < _layerCount; i++)
 	{
@@ -61,15 +65,83 @@ void TFbxImporter::LoadMesh(FbxNode* node, TFbxMesh& mesh)
 		{
 			_vertexUVList.push_back(_layer->GetUVs());
 		}
+		if (_layer->GetVertexColors() != nullptr)
+		{
+			_vertexColorList.push_back(_layer->GetVertexColors());
+		}
+		if (_layer->GetNormals() != nullptr)
+		{
+			_vertexNormalList.push_back(_layer->GetNormals());
+		}
+		if (_layer->GetMaterials() != nullptr)
+		{
+			_materialList.push_back(_layer->GetMaterials());
+		}
 
 	}
 
+	int mtrlNum = node->GetMaterialCount();
+
+	if (mtrlNum > 1)
+	{
+		mesh.m_triangleList.resize(mtrlNum);
+	}
+	else
+	{
+		mesh.m_triangleList.resize(1);
+	}
+
+	for (int i = 0; i < mtrlNum; i++)
+	{
+		FbxSurfaceMaterial* mSurface = node->GetMaterial(i);
+
+		if (mSurface)
+		{
+			std::string texName = ParseMaterial(mSurface);
+			CHAR drive[MAX_PATH];
+			CHAR dir[MAX_PATH];
+			CHAR fName[MAX_PATH];
+			CHAR ext[MAX_PATH];
+			_splitpath_s(texName.c_str(), drive, MAX_PATH, dir, MAX_PATH, fName, MAX_PATH, ext, MAX_PATH);
+
+			std::string tName = fName;
+			tName += ext;
+			mesh.m_texFileNameList.push_back(mtw(tName));
+		}
+	}
+
+	FbxAMatrix geoMat;
+	FbxAMatrix normalMat;
+	{
+		FbxVector4 s = node->GetGeometricScaling(FbxNode::eSourcePivot);
+		FbxVector4 r = node->GetGeometricRotation(FbxNode::eSourcePivot);
+		FbxVector4 t = node->GetGeometricTranslation(FbxNode::eSourcePivot);
+
+		geoMat.SetS(s);
+		geoMat.SetR(r);
+		geoMat.SetT(t);
+
+		normalMat = geoMat;
+		normalMat = normalMat.Inverse();
+		normalMat = normalMat.Transpose();
+	}
+
+	UINT basePolyIndex = 0;
+
 	for (int i = 0; i < _polygonCount; i++)
 	{
+		int subMtrl = 0;
+		if (mtrlNum >= 1)
+		{
+			subMtrl = GetSubMaterialIndex(i, _materialList[0]);
+		}
+
 		UINT _polygonSize = _mesh->GetPolygonSize(i);
 		UINT _triangleCount = _polygonSize - 2;
 
 		UINT _cornerIndices[3];
+
+		mesh.m_PolygonSize += _triangleCount;
 
 		for (int j = 0; j < _triangleCount; j++)
 		{
@@ -82,18 +154,45 @@ void TFbxImporter::LoadMesh(FbxNode* node, TFbxMesh& mesh)
 			_vUv[1] = _mesh->GetTextureUVIndex(i, j + 2);
 			_vUv[2] = _mesh->GetTextureUVIndex(i, j + 1);
 
+			UINT _vColor[3];
+			_vColor[0] = 0;
+			_vColor[1] = j + 2;
+			_vColor[2] = j + 1;
+
 			for (int k = 0; k < 3; k++)
 			{
 				UINT _idcIndex = _cornerIndices[k];
 				PT_Vertex _pnct;
 
+				//vertex
 				FbxVector4 vx = _vertexPos[_idcIndex];
 				_pnct.p.x = vx.mData[0];
 				_pnct.p.y = vx.mData[2];
 				_pnct.p.z = vx.mData[1];
-				_pnct.n = { 0,0,0 };
-				_pnct.c = { 1,1,1,1 };
 
+				//normal
+				_pnct.n = { 0,0,0 };
+				if (_vertexNormalList.size() > 0)
+				{
+					FbxVector4 tNormal = ReadNormal(_vertexNormalList[0], _idcIndex, basePolyIndex + _vColor[k]);
+
+					auto latNormal = normalMat.MultT(tNormal);
+					latNormal.Normalize();
+					_pnct.n.x = latNormal.mData[0];
+					_pnct.n.y = latNormal.mData[1];
+					_pnct.n.z = latNormal.mData[2];
+				}
+				//color
+				_pnct.c = { 1,1,1,1 };
+				if (_vertexColorList.size() > 0)
+				{
+					FbxColor tColor = ReadColor(_vertexColorList[0], _idcIndex, basePolyIndex + _vColor[k]);
+					_pnct.c.x = tColor.mRed;
+					_pnct.c.y = tColor.mGreen;
+					_pnct.c.z = tColor.mBlue;
+					_pnct.c.w = tColor.mAlpha;
+				}
+				//texUV
 				if (_vertexUVList.size() > 0)
 				{
 					FbxLayerElementUV* _uvLayer = _vertexUVList[0];
@@ -103,13 +202,11 @@ void TFbxImporter::LoadMesh(FbxNode* node, TFbxMesh& mesh)
 					_pnct.t.y = 1.0f - (float)uv.mData[1];
 				}
 
-				mesh.m_triangleList.push_back(_pnct);
+				mesh.m_triangleList[subMtrl].push_back(_pnct);
 			}
-
 		}
-
+		basePolyIndex += _polygonSize;
 	}
-
 }
 
 FbxVector2 TFbxImporter::ReadTextureCoord(FbxLayerElementUV* uvLayer, int uvIndex)
@@ -148,6 +245,26 @@ FbxVector2 TFbxImporter::ReadTextureCoord(FbxLayerElementUV* uvLayer, int uvInde
 	return _uv;
 }
 
+FbxColor TFbxImporter::ReadColor(FbxLayerElementVertexColor* lColor, int vIndex, int iIndex)
+{
+	return FbxColor();
+}
+
+FbxVector4 TFbxImporter::ReadNormal(FbxLayerElementNormal* lNormal, int vIndex, int iIndex)
+{
+	return FbxVector4();
+}
+
+C_STR TFbxImporter::ParseMaterial(FbxSurfaceMaterial* sMtrl)
+{
+	return C_STR();
+}
+
+int TFbxImporter::GetSubMaterialIndex(UINT poly, FbxLayerElementMaterial* eMtrl)
+{
+	return 0;
+}
+
 bool TFbxImporter::Init()
 {
 	m_pSDKManager = FbxManager::Create();
@@ -178,9 +295,7 @@ bool TFbxImporter::Clear()
 			m_pMeshNodeList[i]->Destroy();
 			m_pMeshNodeList[i] = nullptr;
 		}
-
 		m_pMeshNodeList.clear();
-
 	}
 
 	if (m_pFbxScene) m_pFbxScene->Clear();
--- LibSolution/TStudyLib/TFbxImporter.h
+++ LibSolution/TStudyLib/TFbxImporter.h
@@ -8,7 +8,11 @@
 
 struct TFbxMesh
 {
-	std::vector<PT_Vertex> m_triangleList;
+	using SubMaterial = std::vector<PT_Vertex>;
+	UINT                     m_PolygonSize = 0;
+	std::vector<SubMaterial> m_triangleList;
+	std::vector<UINT>        m_offsetList;
+	std::vector<W_STR>       m_texFileNameList;
 };
 
 class TFbxImporter
@@ -17,6 +21,7 @@ public:
 	FbxManager*			  m_pSDKManager = nullptr;
 	FbxImporter*		  m_pFbxImporter = nullptr;
 	FbxScene*			  m_pFbxScene = nullptr;
+	FbxNode*			  m_rootNode = nullptr;
 public:
 	std::vector<FbxNode*> m_pMeshNodeList;
 	std::vector<TFbxMesh> m_meshList;
@@ -24,7 +29,12 @@ public:
 	bool	   Load(W_STR filePath);
 	void	   PreProcess(FbxNode* fbxNode);
 	void	   LoadMesh(FbxNode* node, TFbxMesh& mesh);
+public:
 	FbxVector2 ReadTextureCoord(FbxLayerElementUV* uvLayer, int uvIndex);
+	FbxColor   ReadColor(FbxLayerElementVertexColor* lColor, int vIndex, int iIndex);
+	FbxVector4 ReadNormal(FbxLayerElementNormal* lNormal, int vIndex, int iIndex);
+	C_STR      ParseMaterial(FbxSurfaceMaterial* sMtrl);
+	int        GetSubMaterialIndex(UINT poly, FbxLayerElementMaterial* eMtrl);
 public:
 	bool	   Init();
 	bool	   Release();
